d29c046da1348ab0aa5bbb000d904cbc098ab47d
Benjamin <benjamin.kinnard1@gmail.com>
2017-05-10 16:38:05 +0100

    passed concat test(handwritten)

--- array_tasks.js
+++ array_tasks.js
@@ -1,8 +1,14 @@
 var arrayTasks = {
 
-	// concat: function (arr1, arr2) {
-
-	// },
+	concat: function (arr1, arr2) {
+		// var arr3 = [];
+		// arr3 = arr1.concat(arr2);
+		// return arr3;
+		for(item of arr2){
+			arr1.push(item);
+		}
+		return arr1;
+	},
 
 	// insertAt: function (arr, itemToAdd, index) {
 		
